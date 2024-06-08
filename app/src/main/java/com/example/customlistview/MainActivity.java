92fb55de1efeafd9dce8e487d5be67c03e9cef75
shafiulayon16 <shafiul15-14716@diu.edu.bd>
2024-06-08 10:06:39 +0600

    Final Projecct

--- app/src/main/java/com/example/customlistview/MainActivity.java
+++ app/src/main/java/com/example/customlistview/MainActivity.java
@@ -1,15 +1,13 @@
 package com.example.customlistview;
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     ListView list;
-//    private String[] name;
 
 
     String[] maintitle ={
@@ -39,45 +37,32 @@ public class MainActivity extends AppCompatActivity {
         list.setAdapter(adapter);
 
 
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-                // TODO Auto-generated method stub
-//                int i = 0;
-//                String value = name[i];
-//                Intent intent = new Intent(MainActivity.this, FruitActivity.class);
-//                intent.putExtra("FruitName", value);
-//                startActivity(intent);
-//                Toast.makeText(MainActivity.this, value, Toast.LENGTH_LONG).show();
+        list.setOnItemClickListener((parent, view, position, id) -> {
 
 
+            if(position == 0) {
 
-                if(position == 0) {
-                    //code specific to first list item
+                Toast.makeText(getApplicationContext(),"This is Water Melon",Toast.LENGTH_SHORT).show();
+         }
 
-                    Toast.makeText(getApplicationContext(),"This is Water Melon",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 1) {
-                    //code specific to 2nd list item
-                    Toast.makeText(getApplicationContext(),"This is Strawberry",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 2) {
-
-                    Toast.makeText(getApplicationContext(),"This is Kiwi",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 3) {
-
-                    Toast.makeText(getApplicationContext(),"This is Mango",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 4) {
-
-                    Toast.makeText(getApplicationContext(),"This is Pomegranate",Toast.LENGTH_SHORT).show();
-                }
-
+            else if(position == 1) {
+                //code specific to 2nd list item
+                Toast.makeText(getApplicationContext(),"This is Strawberry",Toast.LENGTH_SHORT).show();
             }
+
+            else if(position == 2) {
+
+                Toast.makeText(getApplicationContext(),"This is Kiwi",Toast.LENGTH_SHORT).show();
+            }
+            else if(position == 3) {
+
+                Toast.makeText(getApplicationContext(),"This is Mango",Toast.LENGTH_SHORT).show();
+            }
+            else if(position == 4) {
+
+                Toast.makeText(getApplicationContext(),"This is Pomegranate",Toast.LENGTH_SHORT).show();
+            }
+
         });
     }
 }
